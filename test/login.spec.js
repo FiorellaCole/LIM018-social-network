a9fa8ced0e5c5ab1d2ffb639109db805f923232f
fiorellitamaya <fiorellitamaya@hotmail.com>
2022-08-06 21:25:11 -0500

    Tratando de empezar test

--- test/login.spec.js
+++ test/login.spec.js
@@ -1,18 +1,24 @@
-import { login } from '../src/components/login.js';
-import { signInWithEmailAndPassword } from '../firebase.js';
-jest.mock('../firebase.js' , () => {
+import { logInForm, login } from '../src/components/login.js';
+// import { signInWithEmailAndPassword } from '../firebase.js';
+jest.mock('../src/firebase.js' , () => {
      
-    return {
+  return {
       __esModule: true,
-      ...originalModule,
-      signInWithEmailAndPassword: jest.fn(auth, correo, contraseña) => 'mocked baz',
-    }
+      // ...originalModule,
+      // signInWithEmailAndPassword: jest.fn(auth, correo, contraseña) => 'mocked baz',
+    };
   });
 
 describe('login', () => {
 
 //  signInWithEmailAndPassword(auth, correo, contraseña)
   it('debería ser una función', () => {
-    expect(typeof login).toBe('function');
+    // expect(typeof login).toBe('function');
+    document.body.innerHTML = logInForm();
+    const ingresar = document.getElementById('loginForm');
+    console.log(ingresar.on);
+    login();
+    
+    console.log(ingresar.onclick);
   });
 });
