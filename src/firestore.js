ed8eaae910220758ab0c84638bc18f8e13529418
FiorellaCole <fiorellacoleaguilar@gmail.com>
2022-08-08 11:41:35 -0500

    tratando de crear post

--- src/firestore.js
+++ src/firestore.js
@@ -1,4 +1,15 @@
-import { db, doc, setDoc } from './firebase.js';
+import { 
+  db,
+  doc,
+  setDoc,
+  addDoc,
+  getDoc,
+  query,
+  collection,
+  onSnapshot,
+  updateDoc,
+  orderBy,
+} from './firebase.js';
 
 export async function agregarUsuario(usuario, correo, id) {
   await setDoc(doc(db, 'users', id), {
@@ -6,3 +17,18 @@ export async function agregarUsuario(usuario, correo, id) {
     mail: correo,
   });
 }
+
+export const createPost = (uid, post, datePost, state, likes) => {
+  addDoc(collection(db, 'post'), {
+    uid,
+    post,
+    datePost,
+    state,
+    likes,
+  });
+};
+
+export const getPost = (querySnapshot) => {
+  const queryPost = query(collection(db, 'post'), orderBy('datePost', 'desc'));
+  onSnapshot(queryPost, querySnapshot);
+};
